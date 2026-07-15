6c054ce0075dec24517114c6542df4890045b4ac
ragul <manwaysoftware@gmail.com>
2026-07-15 17:17:36 +0530

    utils updated

---
+++ app.ts
@@ -0,0 +1,24 @@
+import exp from 'express';
+import { globalErrorHandler, resInterspector } from './aop/aop.js';
+import { error } from 'node:console';
+import { server,environment } from './config.js';
+const app: exp.Application = exp();
+
+//add this first line
+app.use(exp.json())
+app.use(resInterspector);
+
+app.get('/', (req: exp.Request, res: exp.Response) => {
+    
+res.status(200).json("hello")
+    
+})
+
+app.listen(server.port, () => {
+    console.log('Server is running on http://localhost:'+JSON.stringify(server)+"environement:"+environment);
+} )      
+
+//added here keps this as a last line
+app.use(globalErrorHandler);
+
+export default app
